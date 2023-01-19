63aa055ee6eb41f5ce256cf76756f0b381cf0a79
Trimikha Valentius <tvalentius@Trimikhas-MacBook-Air.local>
2023-01-19 16:33:13 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,20 @@ import { Refine } from "@pankod/refine-core";
 
 import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-react-router-v6";
+import { HeadlessInferencer } from "@pankod/refine-inferencer/headless";
 
 function App() {
   return (
     <Refine
       dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
       routerProvider={routerProvider}
+      resources={[
+        {
+          name: "products",
+          list: HeadlessInferencer,
+          show: HeadlessInferencer
+        },
+      ]}
     />
   );
 }
